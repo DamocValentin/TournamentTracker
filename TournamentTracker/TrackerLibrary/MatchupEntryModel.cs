a3ef054f5b87630abaadfdae189f6b000549702a
DamocValentin <valentin.damoc@yahoo.com>
2020-12-22 14:10:39 +0200

    Create base Winforms UI

--- TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -9,11 +9,11 @@ namespace TrackerLibrary
     public class MatchupEntryModel
     {
         /// <summary>
-        /// Represents one team in the matchup.
+        /// Represents one team in the matchup
         /// </summary>
         public TeamModel TeamCompeting { get; set; }
         /// <summary>
-        /// Represents the score for this particular team.
+        /// Represents the score for this particular team
         /// </summary>
         public double Score { get; set; }
         /// <summary>
